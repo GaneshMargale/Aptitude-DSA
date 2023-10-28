b07f711894f57197c86ab3b7f313c5375ba86f7e
Ganesh Sanjay Margale <108195526+GaneshMargale@users.noreply.github.com>
2023-10-28 10:42:31 +0530

    Update resultController.js

--- Controller/resultController.js
+++ Controller/resultController.js
@@ -2,6 +2,7 @@ const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Result = require('../Models/resultModel');
+const Profile = require('../Models/userProfileModel');
 
 exports.getAllResults = catchAsync(async (req, res, next) => {
   const features = new APIFeatures(Result.find(), req.query)
@@ -11,6 +12,11 @@ exports.getAllResults = catchAsync(async (req, res, next) => {
     .paginate();
 
   const results = await features.query;
+
+  results.forEach((document) => {
+    document.Results.sort((a, b) => b.points - a.points);
+  });
+
   console.log(results);
   res.status(200).json({
     status: 'success',
@@ -31,7 +37,7 @@ exports.getResult = catchAsync(async (req, res, next) => {
       'Results.$': 1,
     }
   );
-  console.log(result);
+
   if (!result || !result.Results.length) {
     return next(new AppError('Result not found', 404));
   }
@@ -45,12 +51,50 @@ exports.getResult = catchAsync(async (req, res, next) => {
 });
 
 exports.createResult = catchAsync(async (req, res, next) => {
-  const newResult = await Result.create(req.body);
-
-  res.status(200).json({
-    status: 'success',
-    results: newResult,
+  const existingContest = await Result.findOne({
+    contestNumber: req.params.contestNumber,
   });
+  if (!existingContest) {
+    const defaultResult = {
+      contestNumber: req.params.contestNumber,
+      Results: [],
+    };
+
+    await Result.create(defaultResult);
+  }
+
+  next();
+});
+
+exports.updateAptitudeResult = catchAsync(async (req, res, next) => {
+  const profile = await Profile.findOne({ usn: req.params.usn });
+
+  if (!profile) {
+    return next(new AppError('Profile not found', 404));
+  }
+
+  const result = await Result.findOneAndUpdate(
+    {
+      contestNumber: req.params.contestNumber,
+    },
+    {
+      $push: {
+        Results: {
+          usn: req.params.usn,
+          name: profile.name,
+          points: req.body.points,
+        },
+      },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!result) {
+    return next(new AppError('Question not found', 404));
+  }
 });
 
 exports.deleteResult = catchAsync(async (req, res, next) => {
